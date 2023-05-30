7f5a5710e3c38ec6e4bb5f86a74928dc6e08a2eb
Fernando Ayats <ayatsfer@gmail.com>
2023-05-30 09:32:55 +0200

    fix hm-shim

--- modules/nixos/hm-shim.nix
+++ modules/nixos/hm-shim.nix
@@ -1,11 +1,3 @@
 {lib, ...}: {
-  environment.extraInit =
-    lib.concatMapStringsSep "\n" (file: ''
-      if [ -f ${file} ]; then
-        . ${file}
-      fi
-    '') [
-      "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
-      "/nix/var/nix/profiles/per-user/$USER/profile/etc/profile.d/hm-session-vars.sh"
-    ];
+  environment.extraInit = lib.fileContents ../home-manager/hm-shim.sh;
 }
