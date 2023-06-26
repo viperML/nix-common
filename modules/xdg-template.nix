68afba373e35715411a3c3d7f59532b653ed5608
Fernando Ayats <ayatsfer@gmail.com>
2023-06-26 08:48:50 +0200

    tweak pythonrc

--- modules/xdg-template.nix
+++ modules/xdg-template.nix
@@ -52,25 +52,7 @@ in {
     init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
   '';
 
-  pythonrc.text = ''
-    import os
-    import atexit
-    import readline
-
-    history = os.path.join(os.environ['XDG_CACHE_HOME'], 'python_history')
-    try:
-        readline.read_history_file(history)
-    except OSError:
-        pass
-
-    def write_history():
-        try:
-            readline.write_history_file(history)
-        except OSError:
-            pass
-
-    atexit.register(write_history)
-  '';
+  pythonrc.text = builtins.readFile ./pythonrc;
 
   user-dirs.text = ''
     XDG_DESKTOP_DIR="$HOME/Desktop"
