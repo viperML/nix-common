d5a2020276ec3d5b042d08cf008d667881327266
Fernando Ayats <ayatsfer@gmail.com>
2024-09-03 18:39:56 +0200

    revert root-clean changes

--- modules/nixos/root-clean.nix
+++ modules/nixos/root-clean.nix
@@ -1,5 +1,14 @@
-{lib, ...}: {
-  users.users.root.home = lib.mkForce "/var/empty";
+{
+  fileSystems."/root" = {
+    fsType = "tmpfs";
+    device = "none";
+    options = [
+      "defaults"
+      "size=100M"
+      "mode=0700"
+    ];
+    neededForBoot = true;
+  };
 
   systemd.tmpfiles.rules = [
     "D /nix/var/nix/profiles/per-user/root 755 root root - -"
